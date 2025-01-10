8404c5940dd4997533815431ee05bd6553a0b594
Isaac Holanda <116327518+Mudugo@users.noreply.github.com>
2025-01-10 17:06:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -153,13 +153,13 @@ def gerar_pdf(dados, is_vr=False, parcelas=None, imagem_path=None):
 
     pdf.set_font('Arial', 'B', 16)
     titulo1 = f"{dados['nome']}"
-    pdf.cell(200, 10, txt=titulo1, ln=True, align='C')
+    pdf.cell(200, 20, txt=titulo1, ln=True, align='C')
 
     pdf.ln(10)  # Quebra de linha
     
     pdf.set_font('Arial', 'B', 16)
     titulo2 = f"{{dados['data_admissao']}"
-    pdf.cell(200, 10, txt=titulo2, ln=True, align='C')
+    pdf.cell(200, 30, txt=titulo2, ln=True, align='C')
 
     # Adicionar informações do funcionário
     pdf.set_font('Arial', '', 12)
